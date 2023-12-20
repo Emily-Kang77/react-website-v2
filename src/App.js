e3d773b170347705d97d77499e3d7a6026171891
EmilyKang-77 <emilykang135@gmail.com>
2023-12-20 14:55:56 -0800

    Fix Router paths by adding baseName

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 // import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import About from "./pages/About";
-import Projects from "./pages/Projects";
+import Projects from "./pages/Projects.js";
 import NoPage from "./pages/NoPage";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/react-website-v2">
         <Routes>
-          <Route path="/react-website-v2" element={<About />} />
-          <Route path="/react-website-v2/about" element={<About />}/>
-          <Route path="/react-website-v2/projects" element={<Projects />}/>
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />}/>
+          <Route path="/projects" element={<Projects />}/>
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
