f4c8671c2b28a6704af9fd06365602114c589380
EmilyKang-77 <emilykang135@gmail.com>
2023-12-20 17:01:08 -0800

    Use hash router instead of browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import { HashRouter, Routes, Route } from "react-router-dom"; 
 import About from "./pages/About";
 import Projects from "./pages/Projects.js";
 import NoPage from "./pages/NoPage";
@@ -8,14 +8,14 @@ import NoPage from "./pages/NoPage";
 function App() {
   return (
     <div>
-      <BrowserRouter basename="/react-website-v2">
+      <HashRouter basename="/react-website-v2">
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/about" element={<About />}/>
           <Route path="/projects" element={<Projects />}/>
           <Route path="*" element={<NoPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
