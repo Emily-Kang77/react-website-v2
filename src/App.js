ca8314627cb2d2d59ac9c96584b1a13652ac5a0d
EmilyKang-77 <emilykang135@gmail.com>
2023-12-20 18:05:32 -0800

    Get HashRouter to work properly

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
 // import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom"; 
+import { Routes, Route } from "react-router-dom"; 
 import About from "./pages/About";
-import Projects from "./pages/Projects.js";
+import Projects from "./pages/Projects";
 import NoPage from "./pages/NoPage";
 
 function App() {
   return (
     <div>
-      <HashRouter basename="/react-website-v2">
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/about" element={<About />}/>
           <Route path="/projects" element={<Projects />}/>
           <Route path="*" element={<NoPage />} />
         </Routes>
-      </HashRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
+
+// import { createHashRouter, RouterProvider } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const router = createHashRouter([
+//   {
+//     path: "/*",
+//     element: <App />,
+//   }
+// ])
+
 root.render(
-  <App />
+  <React.StrictMode>
+    <HashRouter basename="/react-website-v2">
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+  
 );
