08c4a4b4dd493a2ded576c545a9b00fa72130703
EmilyKang-77 <emilykang135@gmail.com>
2024-01-08 21:23:46 -0800

    Update introductions

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,17 +10,18 @@ export default function renderAboutPage () {
                 <h2>Intro</h2>
 
                 <p>Hi, I'm Emily and I'm a junior in CS at the University of Washington. I'm interested in security and
-                full stack web development. I'm just exploring areas to see what I like. I've led a couple of group projects before because of my initiative to learn 
-                and support each team I was in. See more <a href="projects">here</a>!</p>
+                full stack web development. Currently, I'm just exploring roles to see what I like. I've led a couple of group projects before because of my initiative to learn 
+                and I did my best to support each team I was in. See more <a href="projects">here</a>!</p>
 
-                <p>I have a 2024 SWE internship at Boeing again, with some opportunity for web development, data visualization and 
-                databases.</p>
-
-                <h2>Relevant Classes</h2>
+                <h2>Relevant Classes 2023-2024</h2>
 
                 <ul>
-                    <li>CSE 331 (though I wish I took CSE 154 last Spring..)</li>
-                    <li>CSE 163 (Python Data Programming, if data visualization is ever relevant)</li>
+                    <li>CSE 344 - Introduction to Data Management (Databases)</li>
+                    <li>CSE 333 - Systems Programming in C++</li>
+                    <li>CSE 332 - Data Structures and Algorithms</li>
+                    <li>CSE 351 - Hardware and Software Interface </li>
+                    <li>CSE 331 - Software Design and Implementation </li>
+                    <li>CSE 163 - Python Data Programming</li>
                 </ul>
 
                 
